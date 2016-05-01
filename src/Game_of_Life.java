7ab5a9e61d334d80d06012cc28cf95648b095871
Mohamad Denno <dennokhmhd@gmail.com>
2016-05-01 15:52:49 +0200

    new comments

--- src/Game_of_Life.java
+++ src/Game_of_Life.java
@@ -29,9 +29,9 @@ public class Game_of_Life {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hi Sony");
-		System.out.println("Hello from eclipseAli");
-		System.out.println("Fick Boris");
+		//initialize the grid 101*82
+		//input methods (1->5)
+		//print the choice on the grid
 	}
 	
 
