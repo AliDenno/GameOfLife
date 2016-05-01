4a42dccde40949002440cec86c0a4b959f17ba1d
Ali Denno <aliedenno@gmail.com>
2016-05-01 15:48:50 +0200

    Fix: Fix conflict

--- src/Game_of_Life.java
+++ src/Game_of_Life.java
@@ -36,24 +36,4 @@ public class Game_of_Life {
 	
 
 }
-<<<<<<< cb80404b3cb780efc3cf07293968210aa9493375
 
-=======
-
-
-public class Game_of_Life {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hi Sony");
-		System.out.println("Hello from eclipseAli");
-		System.out.println("Fick Boris");
-		System.out.println("Egit test");
-		//Initialize array
-				int[][] Grid = new int[101][82];
-	}
-
-}
-
-
->>>>>>> 61d089fe97d099db0437242aaafac6de70ebe4bb
