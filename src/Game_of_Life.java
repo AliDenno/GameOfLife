9e91f71eb400285266a96689d29cc767e848750c
Ali Denno <aliedenno@gmail.com>
2016-05-01 15:08:53 +0200

    Add: greetings from eclipse

--- src/Game_of_Life.java
+++ src/Game_of_Life.java
@@ -4,6 +4,7 @@ public class Game_of_Life {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi Sony");
+		System.out.println("Hello from eclipseAli");
 	}
 
 }
