937f5eacd984d096fc2447bc4e8325a45b017918
Ali Denno <aliedenno@gmail.com>
2016-05-01 11:16:49 +0200

    Add: greetings

--- src/FireEveryWhere.java
+++ src/FireEveryWhere.java
@@ -4,6 +4,7 @@ public class FireEveryWhere {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("hi sony");
+		System.out.print("hi SmallSony");
 		//gfdgsdfgsdfgsd
 	}
 
