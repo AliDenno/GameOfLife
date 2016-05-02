083591cb6382c0f9ee2be33f1d9a5d9e0eaa9c9f
Ali Denno <aliedenno@gmail.com>
2016-05-02 19:15:17 +0200

    Add: glider_Gun

--- src/Game_of_Life.java
+++ src/Game_of_Life.java
@@ -32,13 +32,12 @@ public class Game_of_Life {
 		grid[row/2+2][row/2]=1;
 		return grid;
 	}
-	// Ali Is Working on this 
 	public static int[][] glider_Gun(int[][] grid)
 	{
 		//print a glider_Gun on the initial grid
 		grid[5][1]=1;
 		grid[5][2]=1;
-		grid[6][2]=1;
+		grid[6][1]=1;
 		grid[6][2]=1;
 		
 		grid[3][35]=1;
@@ -46,8 +45,39 @@ public class Game_of_Life {
 		grid[4][35]=1;
 		grid[4][36]=1;
 		
+		grid[3][21]=1;
+		grid[3][22]=1;
+		grid[4][21]=1;
+		grid[4][22]=1;
+		grid[5][21]=1;
+		grid[5][22]=1;
 		
+		grid[2][23]=1;
+		grid[6][23]=1;
 		
+		grid[1][25]=1;
+		grid[2][25]=1;
+		grid[6][25]=1;
+		grid[7][25]=1;
+		
+		grid[5][11]=1;
+		grid[6][11]=1;
+		grid[7][11]=1;
+		grid[4][12]=1;
+		grid[8][12]=1;
+		
+		grid[3][13]=1;
+		grid[9][13]=1;
+		grid[3][14]=1;
+		grid[9][14]=1;
+		
+		grid[6][15]=1;
+		grid[4][16]=1;
+		grid[8][16]=1;
+		grid[6][18]=1;
+		grid[5][17]=1;
+		grid[6][17]=1;
+		grid[7][17]=1;
 		return grid;
 	}
 	public static int[][] your_own_Choice(int[][] grid)
