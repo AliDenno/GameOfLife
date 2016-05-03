794795830b9acd98593d21c32d4391558b1c5c2e
Ali Denno <aliedenno@gmail.com>
2016-05-03 20:33:35 +0200

    Add: a set of 3 methods that would conclude the project with a possibility of some bugs

--- src/Game_of_Life.java
+++ src/Game_of_Life.java
@@ -189,16 +189,202 @@ public class Game_of_Life {
 		return result;
 	}
 	
+	/** a function that operate on every cell of the grid and calculate the next value
+	  * 
+	  * @param The Grid
+	  * @param The Boolean Grid 
+	  *
+	*/
+	public static boolean[][] operateGrid(int[][] grid,boolean[][] gridstats ){
+		
+		int A1=-1;int A2=-1; int A3=-1;int B1=-1;int B2=-1;int B3=-1;int C1=-1;int C2=-1;int C3=-1;
+		for(int row=0;row < grid.length;row++){
+			for(int col=0; col < grid[row].length;col++){
+				//corner points
+				if(row==0)
+				{
+					//left top corner
+					if(col==0){
+						A1=grid[grid.length-1][grid[row].length-1];
+						A2=grid[grid.length-1][0];
+						A3=grid[grid.length-1][1];
+						B1=grid[row][grid[row].length-1];
+						B2=grid[row][col];
+						B3=grid[row][col+1];
+						C1=grid[row+1][grid[row].length-1];
+						C2=grid[row+1][col];
+						C3=grid[row+1][col+1];
+					}
+					//right top corner
+					else if(col==grid[row].length-1){
+						A1=grid[grid.length-1][grid[row].length-2];
+						A2=grid[grid.length-1][grid[row].length-1];
+						A3=grid[grid.length-1][0];
+						B1=grid[0][grid[row].length-2];
+						B2=grid[0][grid[row].length-1];
+						B3=grid[0][0];
+						C1=grid[row+1][grid[row].length-2];
+						C2=grid[row+1][grid[row].length-1];
+						C3=grid[row+1][0];
+					}
+					// on first row
+					else{
+						A1=grid[grid.length-1][col-1];
+						A2=grid[grid.length-1][col];
+						A3=grid[grid.length-1][col+1];
+						B1=grid[row][col-1];
+						B2=grid[row][col];
+						B3=grid[row][col+1];
+						C1=grid[row+1][col-1];
+						C2=grid[row+1][col];
+						C3=grid[row+1][col+1];
+					}
+				}
+					
+				else if(row==grid.length-1)
+				{
+					//left bottom corner
+					if(col==0)
+					{
+						A1=grid[grid.length-2][grid[row].length-1];
+						A2=grid[grid.length-2][0];
+						A3=grid[grid.length-2][1];
+						B1=grid[grid.length-1][grid[row].length-1];
+						B2=grid[grid.length-1][0];
+						B3=grid[grid.length-1][1];
+						C1=grid[0][grid[row].length-1];
+						C2=grid[0][0];
+						C3=grid[0][col+1];
+					}
+					//right bottom corner
+					else if(col==grid[row].length-1)
+					{
+						A1=grid[grid.length-2][grid[row].length-2];
+						A2=grid[grid.length-2][grid[row].length-1];
+						A3=grid[grid.length-2][0];
+						B1=grid[grid.length-1][grid[row].length-2];
+						B2=grid[grid.length-1][grid[row].length-1];
+						B3=grid[grid.length-1][0];
+						C1=grid[0][grid[row].length-2];
+						C2=grid[0][grid[row].length-1];
+						C3=grid[0][0];
+					}
+					// on last row
+					else{
+						A1=grid[grid.length-2][col-1];
+						A2=grid[grid.length-2][col];
+						A3=grid[grid.length-2][col+1];
+						B1=grid[grid.length-1][col-1];
+						B2=grid[grid.length-1][col];
+						B3=grid[grid.length-1][col+1];
+						C1=grid[0][col-1];
+						C2=grid[0][col];
+						C3=grid[0][col+1];
+					}
+				}
+				// on left column
+				else if(col==0)
+				{
+					A1=grid[row-1][grid[row].length-1];
+					A2=grid[row-1][col];
+					A3=grid[row-1][col+1];
+					B1=grid[row][grid[row].length-1];
+					B2=grid[row][col];
+					B3=grid[row][col+1];
+					C1=grid[row+1][grid[row].length-1];
+					C2=grid[row+1][col];
+					C3=grid[row+1][col+1];
+				}
+				// on right column
+				else if(col==grid[row].length-1)
+				{
+					A1=grid[row-1][col-1];
+					A2=grid[row-1][col];
+					A3=grid[row-1][0];
+					B1=grid[row][col-1];
+					B2=grid[row][col];
+					B3=grid[row][0];
+					C1=grid[row+1][col-1];
+					C2=grid[row+1][col];
+					C3=grid[row+1][0];
+				}
+				// in middle
+				else{
+					A1=grid[row-1][col-1];
+					A2=grid[row-1][col];
+					A3=grid[row-1][col+1];
+					B1=grid[row][col-1];
+					B2=grid[row][col];
+					B3=grid[row][col+1];
+					C1=grid[row+1][col-1];
+					C2=grid[row+1][col];
+					C3=grid[row+1][col+1];
+				}
+				gridstats[row][col]=assignStatus(A1,A2,A3,B1,B2,B3,C1,C2,C3);
+			}
+		}
+		return gridstats;
+	}
+	
+	/** a function that assign a boolean value to the cell that is under processing
+	  * 
+	  * @param The cell and its surrounding 
+	  *
+	*/
+	public static boolean assignStatus(int A1,int A2,int A3,int B1,int B2,int B3,int C1,int C2,int C3)
+	{
+		boolean flag=false;
+		int sum =  A1+ A2+ A3+ B1+ B3+ C1+ C2+ C3;
+		
+		if(B2==1){
+			//Survival
+			if (sum == 2 || sum == 3)
+				flag=true;
+			//Death
+			else if (sum>3||sum<2)
+				flag=false;
+		}
+		//Birth
+		else if(B2==0 && sum==3){
+			flag=true;
+		}
+		
+		return flag;
+	}
+	
+	/** a function that check the boolean grid and update the actual grid according to the values
+	  * 
+	  * @param The grid of data 
+	  * @param The grid of boolean values 
+	  *
+	  *
+	*/
+	public static int[][] updateGrid(int[][] grid,boolean[][] gridstats)
+	{
+		for(int row=0;row < grid.length;row++){
+			for(int col=0; col < grid[row].length;col++){
+				if(gridstats[row][col]){
+					grid[row][col]=1;
+				}
+				else{
+					grid[row][col]=0;
+				}
+			}
+		}
+		return grid;
+	}
+	
+	
 	/** a function that takes the grid and write it to a file
 	  * 
 	  * @param The grid of data 
 	  *
 	*/
-	public static void save_2DArray_tofile(int[][] grid)
+	public static void save_2DArray_tofile(int[][] grid,String filename)
 	{
 		PrintStream ps;
 		try {
-		ps = new PrintStream(new FileOutputStream("Grid.DATA"));
+		ps = new PrintStream(new FileOutputStream(filename));
 		for(int row=0;row < grid.length;row++){
 			for(int col=0; col < grid[row].length;col++){
 			String s = Integer.toString(grid[row][col]);
@@ -211,8 +397,10 @@ public class Game_of_Life {
 		System.out.println(e.getMessage());
 		}
 	}
+	
 	static int row=101;
 	static int col=82;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner reader = new Scanner(System.in);  // Reading from System.in
@@ -232,7 +420,18 @@ public class Game_of_Life {
 		
 		// 2D 101 x 82 sized array
 		int[][] Grid = new int[row][col];
-	
+		boolean [][] GridStats = new boolean [row][col];
+		/////////////////////////////////////////////////
+		/*
+		Grid[0][0]=1;
+		Grid[0][Grid[0].length-1]=	2;
+		Grid[Grid.length-1][0]=3;
+		Grid[Grid.length-1][Grid[0].length-1]=	4;
+		save_2DArray_tofile(Grid);
+		System.out.println("Wrote");
+		System.exit(0);
+		*/
+		/////////////////////////////////////////////////
 		switch (choice) {
 		case 0:  break;
         case 1:  System.out.println("1.blinker launched");
@@ -252,7 +451,10 @@ public class Game_of_Life {
         		 break;
 
 		}
-		save_2DArray_tofile(Grid);
+		save_2DArray_tofile(Grid,"Grid.Data");
+		GridStats = operateGrid(Grid,GridStats);
+		updateGrid(Grid,GridStats);
+		save_2DArray_tofile(Grid,"UGrid.Data");
 		System.out.println("NUMMUM: "+ alive_cells_count(0,Grid) );
 	}
 }
